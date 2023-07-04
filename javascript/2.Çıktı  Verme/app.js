36b7e877e6bca1b3e6a793e75d15ff07a3c3a872
EFK1903 <120033055+EFK1903@users.noreply.github.com>
2023-07-04 16:27:53 +0300

    Javascript

---
+++ javascript/2.Çıktı  Verme/app.js
@@ -0,0 +1,32 @@
+// Document write ile çıktı verme
+
+// document.write("Hello World <br>");
+// document.write("Deneme <br>");
+// document.write(5.5);
+
+
+// Console log metodu
+
+// console.log("Hello World");
+// console.log("Deneme");
+// console.log(19.03);
+// console.log(true);
+// console.log({name:"Enes",surname: "Keleş"});
+
+// console.clear();
+
+// let a=5;
+// let b=10;
+
+// console.log("İki Sayının Toplamı: ", a+b);
+
+// console.clear();
+
+// Alert (Uyarı) pop-up ile çıktı verme
+
+// console.log(window);
+
+// let a=10;
+// let b=7;
+
+// alert("İki Sayının Toplamı : " + (a+b));
