dc79527fca5ca4240976e8fe6dbc344e043e1e8e
EFK1903 <120033055+EFK1903@users.noreply.github.com>
2023-07-06 17:07:50 +0300

    Add files via upload

---
+++ javascript/10.Koşul Yapıları/app.js
@@ -0,0 +1,41 @@
+// Koşul Yapıları
+
+// let not = 65;
+
+// if(not>50){
+//     console.log("Geçtiniz , notunuz :" + not);
+// }
+// else{
+//     console.log("Kaldınız , notunuz :" + not);
+// }
+
+// let yas = Number(prompt("Yaşınız:"));
+// console.log(typeof yas);
+// let para = Number(prompt("Bütçeniz :"));
+
+// if(yas>=18 && para>=3000){
+//     alert("Ehliyet Sınavına Katılabilirsiniz");
+// }
+// else{
+//     alert("Ehliyet Sınavına Katılamazsınız");
+// }
+
+
+// ------ Ders Ortalaması Bulma ------
+
+// vize1 %30 vize2 %30 final %40
+
+// let vize1 = Number(prompt("Vize 1 Notunuzu Giriniz :"));
+// let vize2 = Number(prompt("Vize 2 Notunuzu Giriniz :"));
+// let final = Number(prompt("Final Notunuzu Giriniz :"));
+
+// let ortalama = (vize1*0.3) + (vize2*0.3) + (final*0.4);
+
+// if(ortalama>=50){
+//     alert("Dersten Geçtiniz Tebrikler! -->" + ortalama);
+//     console.log("Dersten Geçtiniz Tebrikler!");
+// }
+// else{
+//     alert("Dersten Kaldınız! -->" + ortalama);
+//     console.log("Dersten Kaldınız!");
+// }
---
+++ javascript/9.Diyalog Kutuları/app.js
@@ -0,0 +1,35 @@
+// Diyalog Kutuları
+
+/* 
+
+Alert
+Prompt
+Confirm
+
+*/
+
+// Alert
+
+// console.log(window);
+
+// alert("İşleminize devam etmeden önce kaydetmelisiniz.");
+
+// Prompt
+
+// prompt(); ile kullanıcıdan değer alabiliyoruz. 
+// Kullanıcıdan alınan değerler her zaman string döner.
+
+// let isim = prompt("İsminizi Giriniz :");
+// let yas = prompt("Yasınızı giriniz :");
+
+// console.log("İsmininiz : " + isim);
+// console.log("Yaşınız : " + yas);
+
+// console.log(typeof isim);
+// console.log(typeof yas);
+
+
+// Confirm
+
+// let sonuc = confirm("Silmek istediğinize emin misiniz?");
+// console.log(sonuc);
