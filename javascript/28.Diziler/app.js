8bc87ae11a1a4d1287996a64e4418c79fb5e5fce
EFK1903 <120033055+EFK1903@users.noreply.github.com>
2023-07-12 18:16:12 +0300

    Add files via upload

---
+++ javascript/28.Diziler/app.js
@@ -0,0 +1,14 @@
+// Dizilere Giriş
+
+// let sayilar = [0,1,2,3,4,5,"Enes",7,8,9"Ali"];
+// sayilar[sayilar.length-1]= "Mehmet";
+// console.log(sayilar[10]);
+
+
+// let isimler = ["Enes", "Furkan", "Büşra" "Yusuf"];
+// isimler[2] = "Caner";
+// console.log(isimler);
+
+
+// let karisikDİzi = [1, "Enes", 6.1, true, null, undefined];
+// console.log(karisikDİzi[3]);
---
+++ javascript/29.ForEach Döngüsü/app.js
@@ -0,0 +1,21 @@
+// ForEach Döngüsü
+
+let isimler = ["Enes", "Furkan", "Ümit", "Ensar", "Hüseyin"];
+
+// isimler.forEach(function(isim){
+//     console.log(isim);
+// });
+
+
+// for(let i=0 ; i<isimler.length ; i++){
+//     console.log(isimler[i]);
+// }
+
+
+// let sayac = 0;
+// while(sayac<isimler.length){
+//     console.log(isimler[sayac]);
+//     sayac++;
+// }
+
+// Üç Kodun da Çıktısı Aynıdır
